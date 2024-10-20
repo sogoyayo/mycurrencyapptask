936fdbe88c1de445374c971fdb5b8e59ae0e4e03
Israel Oyerinde <sogo.oyerinde@gmail.com>
2024-10-20 20:22:54 +0100

    Initial commit

--- src/components/horizontal-scroll-container.tsx
+++ src/components/horizontal-scroll-container.tsx
@@ -1,14 +1,17 @@
 interface HorizontalScrollSnapContainerProps {
   children: React.ReactNode;
   className?: string;
+  title?: string;
 }
 
 export const HorizontalScrollSnapContainer = ({
   children,
   className = "",
+  title,
 }: HorizontalScrollSnapContainerProps) => {
   return (
     <div className={`relative my-14 py-14 ${className}`}>
+      {title && <h3 className="section-title mb-8 pl-14 lg:pl-56">{title}</h3>}
       <div className="flex gap-4 snap-x scroll-pl-14 lg:scroll-pl-56 space-x-4 overflow-x-auto py-6 px-52">
         {children}
       </div>
--- src/components/product-review.tsx
+++ src/components/product-review.tsx
@@ -4,16 +4,18 @@ import { HorizontalScrollSnapContainer } from "./horizontal-scroll-container";
 
 export const ProductReview = () => {
   return (
-    <HorizontalScrollSnapContainer>
-      {products_review.map((item) => (
-        <div key={item.id} className="flex-none snap-start w-[70%] sm:w-[20rem]">
-          <ProductReviewCard
-            name={item.name}
-            testimonial={item.testimonial}
-            rating={item.rating}
-          />
-        </div>
-      ))}
-    </HorizontalScrollSnapContainer>
+    <>
+      <HorizontalScrollSnapContainer title="Product Reviews">
+        {products_review.map((item) => (
+          <div key={item.id} className="flex-none snap-start w-[70%] sm:w-[20rem]">
+            <ProductReviewCard
+              name={item.name}
+              testimonial={item.testimonial}
+              rating={item.rating}
+            />
+          </div>
+        ))}
+      </HorizontalScrollSnapContainer>
+    </>
   );
 };
