54fdf5d9a940cd21e2681bcdabb1fc3b44533c38
Israel Oyerinde <sogo.oyerinde@gmail.com>
2024-10-20 21:09:18 +0100

    footer responsiveness

--- src/components/horizontal-scroll-container.tsx
+++ src/components/horizontal-scroll-container.tsx
@@ -11,8 +11,8 @@ export const HorizontalScrollSnapContainer = ({
 }: HorizontalScrollSnapContainerProps) => {
   return (
     <div className={`relative my-14 py-14 ${className}`}>
-      {title && <h3 className="section-title mb-8 pl-14 lg:pl-56">{title}</h3>}
-      <div className="flex gap-4 snap-x scroll-pl-14 lg:scroll-pl-56 space-x-4 overflow-x-auto py-6 px-52">
+      {title && <h3 className="section-title mb-8 pl-5 md:pl-14 lg:pl-56">{title}</h3>}
+      <div className="flex gap-4 snap-x scroll-pl-14 lg:scroll-pl-56 space-x-4 overflow-x-auto py-6 px-4 sm:px-8 lg:px-52">
         {children}
       </div>
     </div>
